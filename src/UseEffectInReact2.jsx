1f8500783de9f1e485764f6ff836ab85ccd9b3c6
Rahul Patel <rpatel43510+temp1@gmail.com>
2025-06-18 17:20:01 +0530

    add new files useEffect by component or useEffect in react 2 files areaded

--- src/UseEffectInReact2.jsx
+++ src/UseEffectInReact2.jsx
@@ -5,13 +5,20 @@ import UseEffectByComponent from './UseEffectByComponent';
 function UseEffectInReact2() {
     const [counter, setCounter] = useState(0);
     const [data, setData] = useState(0);
+    const [toggle, setToggle] = useState(true);
 
    return (
-       <div>
+    <div>
         <h1>Use Effect hook with Componant</h1>
+        {
+            toggle ? <h1>Hello Toggle</h1> : null
+        }
       <button onClick={()=>setCounter(counter + 1)}>Counter : {counter}</button>
       <button onClick={()=>setData(data + 1)}>Data : {data}</button>
-      <UseEffectByComponent data={data} counter={counter} />
+      <button onClick={()=>(setToggle(!toggle))} >Toggle</button>
+
+      <UseEffectByComponent data={data} counter={counter} toggle={toggle} />
+      
     </div>
 
   )
